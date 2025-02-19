5f44babddcb77bedb2f3886f18a6b2ebb09e1560
sazankaa <satorobi9280@gmail.com>
2025-02-19 19:39:54 +0900

    bigger than 0

--- script.c
+++ script.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[])//./exe 2 6
 {
     if(argc != 3){
         fprintf(stderr, "Usage: %s length\n", argv[0]);
         return(1);
+    }
+
+    if (*(argv+1) <= 0 || *(argv+2) <= 0){
+        fprintf(stderr, "should be bigger than 0!");
+        return(2);
     }
     
 }
