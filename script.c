fefdc2a7d2923558bec421df5db59ca7f2e75c2d
sazankaa <satorobi9280@gmail.com>
2025-02-19 19:57:23 +0900

    bun

--- script.c
+++ script.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "rotate.h"
+#include<math.h>
+#include<stdlib.h>
 
 int main(int argc, char *argv[])//./exe 2 6
 {
@@ -8,10 +10,30 @@ int main(int argc, char *argv[])//./exe 2 6
         return(1);
     }
 
-    if (*(argv+1) <= 0 || *(argv+2) <= 0){
+    char *err1, *err2;
+    int x, y;
+    if((x=(double)strtol(*(argv+1), &err1, 10) <= 0 || *err1 != '\0')){
+        fprintf(stderr, "Illegal Parameter: %s\n", argv[1]);
+        return(2);
+    }
+
+    if((y=(double)strtol(*(argv+2), &err2, 10) <= 0 || *err2 != '\0')){
+        fprintf(stderr, "Illegal Parameter: %s\n", argv[2]);
+        return(2);
+    }
+
+    if (x <= 0 || y <= 0){
         fprintf(stderr, "should be bigger than 0!");
         return(2);
     }
 
-    r
+    rotate(&x, &y);
+
+    double ans;
+    if (((x - y) % 2) == 1 ){
+        ans = x * y;
+    } else {
+        ans = pow(x,y);
+    }
+
 }
